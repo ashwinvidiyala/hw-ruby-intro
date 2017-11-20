f9f1b66cd623e028c57b7e5fd958f6ba54170f81
Ashwin Vidiyala <ashwinvidiyala@gmail.com>
2017-11-20 21:01:05 +0000

    Finish assignment

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -6,30 +6,53 @@ def sum(arr)
   arr.reduce(0,:+)
 end
 
-def max_2_sum arr
-  # YOUR CODE HERE
+def max_2_sum(arr)
+  if arr.length == 0
+    return 0
+  elsif arr.length == 1
+    return arr[0]
+  else
+    return arr.max(2).reduce(:+)
+  end
 end
 
-def sum_to_n? arr, n
-  # YOUR CODE HERE
+def sum_to_n?(arr, n)
+  if arr.empty?
+    return false
+  elsif arr.permutation(2).any? { |a, b| a + b == n }
+    return true
+  else
+    return false
+  end
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  return "Hello, #{name}"
 end
 
-def starts_with_consonant? s
-  # YOUR CODE HERE
+def starts_with_consonant?(word)
+  word.downcase.start_with?('b','c','d','f','g','h','j','k','l',
+  'm','n','p','q','r','s','t','v','w','x','y','z') if !word.empty?
 end
 
-def binary_multiple_of_4? s
-  # YOUR CODE HERE
+def binary_multiple_of_4?(word)
+ word.to_i(2) % 4 == 0
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  attr_accessor :isbn, :price
+  
+  def initialize(isbn, price)
+    @price = price
+    @isbn = isbn
+    raise ArgumentError if isbn == '' || price <= 0
+  end
+  
+  def price_as_string
+    "$#{'%.2f' % @price}"
+  end
 end
